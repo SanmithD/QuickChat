0dc3d5e907fb91a58e6c65eee43dccfb382dad72
SanmithD <sanmithdevadiga7@gmail.com>
2025-05-31 22:07:01 +0530

    project completed

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import 'dotenv/config';
 import express from 'express';
+import path from 'path';
 import connectDB from './lib/db.lib.js';
 import { app, server } from './lib/socket.js';
 import authRouter from './routes/auth.route.js';
@@ -11,6 +12,7 @@ import messageRouter from './routes/message.route.js';
 connectDB();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 app.use(cors({
     origin: 'http://localhost:5173',
@@ -24,6 +26,14 @@ app.use(cookieParser());
 app.use('/api/auth', authRouter);
 app.use('/api/message', messageRouter);
 app.use('/api/favorites', favoriteRouter);
+
+if(process.env.NODE_ENV == "production"){
+    app.use(express.static(path.join(__dirname, "../../client/dist")));
+
+    app.get("*", (req, res)=>{
+        res.sendFile(path.join(__dirname, "../../client","dist","index.html"));
+    })
+}
 
 server.listen(PORT,()=>{
     console.log(`server stated at ${PORT}`);
